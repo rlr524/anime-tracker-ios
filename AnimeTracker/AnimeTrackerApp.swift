d7f84404e97592b1465857b9fe2e00f97ece1d72
Rob Ranf <rob@emiyaconsulting.com>
2023-02-15 14:08:29 -0800

    Initial Commit

---
+++ AnimeTracker/AnimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTrackerApp.swift
+//  AnimeTracker
+//
+//  Created by Rob Ranf on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
